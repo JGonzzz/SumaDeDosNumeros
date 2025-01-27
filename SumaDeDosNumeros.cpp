302857107ae3028c456d68e01dd5741347783122
JGonzzz <131152180+JGonzzz@users.noreply.github.com>
2025-01-26 23:18:25 -0600

    Cpp

---
+++ SumaDeDosNumeros.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+ int num1,num2, suma;
+ cout<<"Suma de dos numeros :D \n";
+ cout<<"Escriba el primer numero: \n";
+ cin>>num1;
+ cout<<"Escriba el segundo numero: \n";
+ cin>>num2;
+ suma=num1+num2;
+ cout<<"La suma de los dos numeros es: "<<suma;
+ return 0;
+}
